d11d7970aae388f3d84a2d00bfdadbcb207246f5
Carlos Maiolino <cmaiolino@redhat.com>
2018-10-19 12:29:26 +0200

    Btrfs: Remove direct usage of fiemap_extent_info
    Still WIP, there is still a single usage of fieinfo

--- fs/btrfs/extent_io.h
+++ fs/btrfs/extent_io.h
@@ -409,8 +409,7 @@ int btree_write_cache_pages(struct address_space *mapping,
 			    struct writeback_control *wbc);
 int extent_readpages(struct address_space *mapping, struct list_head *pages,
 		     unsigned nr_pages);
-int extent_fiemap(struct inode *inode, struct fiemap_extent_info *fieinfo,
-		__u64 start, __u64 len);
+int extent_fiemap(struct inode *inode, struct fiemap_ctx *f_ctx);
 void set_page_extent_mapped(struct page *page);
 
 struct extent_buffer *alloc_extent_buffer(struct btrfs_fs_info *fs_info,
