a2a9515df705b67c47f635e911712afafea7cf97
Carlos Maiolino <cmaiolino@redhat.com>
2018-11-27 11:39:10 +0100

    fs: Move start and length fiemap fields into fiemap_extent_info
    As the overall goal to deprecate fibmap, Christoph suggested a rework of
    the ->fiemap API, in a way we could pass to it a callback to fill the
    fiemap structure (one of these callbacks being fiemap_fill_next_extent).
    
    To avoid the need to add several fields into the ->fiemap method, aggregate
    everything into a single data structure, and pass it along.
    
    This patch isn't suppose to add any functional change, only to update
    filesystems providing ->fiemap() method.
    
    Reviewed-by: Christoph Hellwig <hch@lst.de>
    
    Changelog:
    
    	V5:
    		- Add comments to fi_start and fi_len fields of
    		  fiemap_extent_info
    		- Fix xfs_vn_fiemap indentation

--- fs/hpfs/file.c
+++ fs/hpfs/file.c
@@ -190,9 +190,9 @@ static sector_t _hpfs_bmap(struct address_space *mapping, sector_t block)
 	return generic_block_bmap(mapping, block, hpfs_get_block);
 }
 
-static int hpfs_fiemap(struct inode *inode, struct fiemap_extent_info *fieinfo, u64 start, u64 len)
+static int hpfs_fiemap(struct inode *inode, struct fiemap_extent_info *fieinfo)
 {
-	return generic_block_fiemap(inode, fieinfo, start, len, hpfs_get_block);
+	return generic_block_fiemap(inode, fieinfo, hpfs_get_block);
 }
 
 const struct address_space_operations hpfs_aops = {
